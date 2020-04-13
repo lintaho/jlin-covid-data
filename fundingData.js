a10d8bc477ea5bd7aef8d5ed45d5e70af9612720
Linhao Zhang <linhao.zhang@gmail.com>
2020-04-12 22:50:14 -0400

    funding seed

---
+++ fundingData.js
@@ -0,0 +1,153 @@
+export const featuredOrganizations = [
+  {
+    title: 'Feeding America',
+    description:
+      'The Feeding America food bank network is committed to serving communities and people facing hunger in America during the COVID-19 pandemic. They provide meals to more than 40 million people each year through their 200 food banks, 60,000 food pantries, and meal programs.',
+    link: 'https://www.feedingamerica.org/jeremylin',
+    goal: 50000,
+    raised: 0,
+  },
+  {
+    title: 'Direct Relief',
+    description:
+      'Since COVID-19 first appeared, Direct Relief has delivered 1.4 million masks, 2.6 million gloves, and more than 575,000 other pieces of personal protective equipment to healthcare workers at 1,063 medical institutions in 32 countries worldwide.',
+    link: 'https://www.directrelief.org/jlin7',
+    goal: 50000,
+    raised: 0,
+  },
+];
+
+export const communityOrganizations = [
+  {
+    title: 'CDC Foundation',
+    description:
+      "Serves low-income population in Queens, hospital was reported in NYT, 13 patients died in one day, hospital being overrun by COVID-19 cases. It's one of the worst hit hospitals in NYC and has been gaining national attention for highlight the dire situation hospital staffs are facing. ",
+    link:
+      'https://give4cdcf.org/?utm_source=CDCF&utm_medium=web&utm_campaign=combat-coronavirus',
+    logo: 'fundraiserLogo.png',
+  },
+  {
+    title: 'Apex For Youth',
+    description:
+      "Serves low-income population in Queens, hospital was reported in NYT, 13 patients died in one day, hospital being overrun by COVID-19 cases. It's one of the worst hit hospitals in NYC and has been gaining national attention for highlight the dire situation hospital staffs are facing. ",
+    link: 'https://donate.apexforyouth.org/give/118004/#!/donation/checkout',
+    logo: 'fundraiserLogo.png',
+  },
+  {
+    title: 'Apex For Youth',
+    description:
+      "Serves low-income population in Queens, hospital was reported in NYT, 13 patients died in one day, hospital being overrun by COVID-19 cases. It's one of the worst hit hospitals in NYC and has been gaining national attention for highlight the dire situation hospital staffs are facing. ",
+    link: 'https://donate.apexforyouth.org/give/118004/#!/donation/checkout',
+    logo: 'fundraiserLogo.png',
+  },
+  {
+    title: 'CDC Foundation',
+    description:
+      "Serves low-income population in Queens, hospital was reported in NYT, 13 patients died in one day, hospital being overrun by COVID-19 cases. It's one of the worst hit hospitals in NYC and has been gaining national attention for highlight the dire situation hospital staffs are facing. ",
+    link:
+      'https://give4cdcf.org/?utm_source=CDCF&utm_medium=web&utm_campaign=combat-coronavirus',
+    logo: 'fundraiserLogo.png',
+  },
+  {
+    title: 'Apex For Youth',
+    description:
+      "Serves low-income population in Queens, hospital was reported in NYT, 13 patients died in one day, hospital being overrun by COVID-19 cases. It's one of the worst hit hospitals in NYC and has been gaining national attention for highlight the dire situation hospital staffs are facing. ",
+    link: 'https://donate.apexforyouth.org/give/118004/#!/donation/checkout',
+    logo: 'fundraiserLogo.png',
+  },
+  {
+    title: 'Apex For Youth',
+    description:
+      "Serves low-income population in Queens, hospital was reported in NYT, 13 patients died in one day, hospital being overrun by COVID-19 cases. It's one of the worst hit hospitals in NYC and has been gaining national attention for highlight the dire situation hospital staffs are facing. ",
+    link: 'https://donate.apexforyouth.org/give/118004/#!/donation/checkout',
+    logo: 'fundraiserLogo.png',
+  },
+  {
+    title: 'CDC Foundation',
+    description:
+      "Serves low-income population in Queens, hospital was reported in NYT, 13 patients died in one day, hospital being overrun by COVID-19 cases. It's one of the worst hit hospitals in NYC and has been gaining national attention for highlight the dire situation hospital staffs are facing. ",
+    link:
+      'https://give4cdcf.org/?utm_source=CDCF&utm_medium=web&utm_campaign=combat-coronavirus',
+    logo: 'fundraiserLogo.png',
+  },
+  {
+    title: 'Apex For Youth',
+    description:
+      "Serves low-income population in Queens, hospital was reported in NYT, 13 patients died in one day, hospital being overrun by COVID-19 cases. It's one of the worst hit hospitals in NYC and has been gaining national attention for highlight the dire situation hospital staffs are facing. ",
+    link: 'https://donate.apexforyouth.org/give/118004/#!/donation/checkout',
+    logo: 'fundraiserLogo.png',
+  },
+  {
+    title: 'Apex For Youth',
+    description:
+      "Serves low-income population in Queens, hospital was reported in NYT, 13 patients died in one day, hospital being overrun by COVID-19 cases. It's one of the worst hit hospitals in NYC and has been gaining national attention for highlight the dire situation hospital staffs are facing. ",
+    link: 'https://donate.apexforyouth.org/give/118004/#!/donation/checkout',
+    logo: 'fundraiserLogo.png',
+  },
+  {
+    title: 'Apex For Youth',
+    description:
+      "Serves low-income population in Queens, hospital was reported in NYT, 13 patients died in one day, hospital being overrun by COVID-19 cases. It's one of the worst hit hospitals in NYC and has been gaining national attention for highlight the dire situation hospital staffs are facing. ",
+    link: 'https://donate.apexforyouth.org/give/118004/#!/donation/checkout',
+    logo: 'fundraiserLogo.png',
+  },
+  {
+    title: 'Apex For Youth',
+    description:
+      "Serves low-income population in Queens, hospital was reported in NYT, 13 patients died in one day, hospital being overrun by COVID-19 cases. It's one of the worst hit hospitals in NYC and has been gaining national attention for highlight the dire situation hospital staffs are facing. ",
+    link: 'https://donate.apexforyouth.org/give/118004/#!/donation/checkout',
+    logo: 'fundraiserLogo.png',
+  },
+  {
+    title: 'Apex For Youth',
+    description:
+      "Serves low-income population in Queens, hospital was reported in NYT, 13 patients died in one day, hospital being overrun by COVID-19 cases. It's one of the worst hit hospitals in NYC and has been gaining national attention for highlight the dire situation hospital staffs are facing. ",
+    link: 'https://donate.apexforyouth.org/give/118004/#!/donation/checkout',
+    logo: 'fundraiserLogo.png',
+  },
+  {
+    title: 'Apex For Youth',
+    description:
+      "Serves low-income population in Queens, hospital was reported in NYT, 13 patients died in one day, hospital being overrun by COVID-19 cases. It's one of the worst hit hospitals in NYC and has been gaining national attention for highlight the dire situation hospital staffs are facing. ",
+    link: 'https://donate.apexforyouth.org/give/118004/#!/donation/checkout',
+    logo: 'fundraiserLogo.png',
+  },
+  {
+    title: 'Apex For Youth',
+    description:
+      "Serves low-income population in Queens, hospital was reported in NYT, 13 patients died in one day, hospital being overrun by COVID-19 cases. It's one of the worst hit hospitals in NYC and has been gaining national attention for highlight the dire situation hospital staffs are facing. ",
+    link: 'https://donate.apexforyouth.org/give/118004/#!/donation/checkout',
+    logo: 'fundraiserLogo.png',
+  },
+  {
+    title: 'Apex For Youth',
+    description:
+      "Serves low-income population in Queens, hospital was reported in NYT, 13 patients died in one day, hospital being overrun by COVID-19 cases. It's one of the worst hit hospitals in NYC and has been gaining national attention for highlight the dire situation hospital staffs are facing. ",
+    link: 'https://donate.apexforyouth.org/give/118004/#!/donation/checkout',
+    logo: 'fundraiserLogo.png',
+  },
+  {
+    title: 'CDC Foundation',
+    description:
+      "Serves low-income population in Queens, hospital was reported in NYT, 13 patients died in one day, hospital being overrun by COVID-19 cases. It's one of the worst hit hospitals in NYC and has been gaining national attention for highlight the dire situation hospital staffs are facing. ",
+    link:
+      'https://give4cdcf.org/?utm_source=CDCF&utm_medium=web&utm_campaign=combat-coronavirus',
+    logo: 'fundraiserLogo.png',
+  },
+  {
+    title: 'CDC Foundation',
+    description:
+      "Serves low-income population in Queens, hospital was reported in NYT, 13 patients died in one day, hospital being overrun by COVID-19 cases. It's one of the worst hit hospitals in NYC and has been gaining national attention for highlight the dire situation hospital staffs are facing. ",
+    link:
+      'https://give4cdcf.org/?utm_source=CDCF&utm_medium=web&utm_campaign=combat-coronavirus',
+    logo: 'fundraiserLogo.png',
+  },
+  {
+    title: 'CDC Foundation',
+    description:
+      "Serves low-income population in Queens, hospital was reported in NYT, 13 patients died in one day, hospital being overrun by COVID-19 cases. It's one of the worst hit hospitals in NYC and has been gaining national attention for highlight the dire situation hospital staffs are facing. ",
+    link:
+      'https://give4cdcf.org/?utm_source=CDCF&utm_medium=web&utm_campaign=combat-coronavirus',
+    logo: 'fundraiserLogo.png',
+  },
+];
